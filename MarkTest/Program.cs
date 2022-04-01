fefdd07fd543fc1c3dd3ba5a6d2a6d89f829f2cf
Иван <mishichev_ia@mail.ru>
2022-04-01 10:04:15 +0800

    Изменения на 01.04.2022

--- MarkTest/Program.cs
+++ MarkTest/Program.cs
@@ -31,33 +31,58 @@ namespace MarkTest
             Socket tSock = o as Socket;
             int n;
             string message = String.Empty;
+            string[] sdata = {
+                "0104607032142591215!:RkV93zn2u",
+                "0104607032142591215!:s*v93Wyyj",
+                "0104607032142591215!;Geg93R0s9",
+                "0104607032142591215!;YXx93pzLd"
+            };
+            int i = 0;
             while (true)
             {
                 try
                 {
-
+                    string words = String.Empty;
                     Socket handler = tSock.Accept();
+                    Console.WriteLine("Accepted: " + handler.RemoteEndPoint.ToString());
+                    if (i > 3)
+                    {
+                        handler.Shutdown(SocketShutdown.Both);
+                        handler.Close();
+                        Console.WriteLine("All data have been sent");
+                        Thread.Sleep(100);
+                        continue;
+                    }
+
+                    message = sdata[i++];
+                    byte[] msg = System.Text.Encoding.UTF8.GetBytes(message + ";;");
+                    n = handler.Send(msg);
+                    if (n > 0)
+                    {
+                        words = Encoding.UTF8.GetString(msg, 0, n);
+                        Console.WriteLine("Sent : " + words);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Data hasn't been sent");
+                    }
+
+                    
+                    byte[] buff = new byte[1024];
                     do
                     {
-                        Console.WriteLine("End Point: " + handler.RemoteEndPoint.ToString());
-                        byte[] buff = new byte[1024];
                         n = handler.Receive(buff);
                         if (n > 0)
                         {
-                            String s1 = Encoding.UTF8.GetString(buff, 0, n);
-                            Console.WriteLine("Received : " + s1 + "\n");
-
-                            Console.WriteLine("Enter message to send: ");
-                            message = Console.ReadLine();
-                            byte[] msg = System.Text.Encoding.UTF8.GetBytes(message);
-                            n = handler.Send(msg);
-                            s1 = Encoding.UTF8.GetString(buff, 0, n);
-                            Console.WriteLine("Sent : " + s1 + "\n");
+                            words = Encoding.UTF8.GetString(buff, 0, n);
+                            Console.WriteLine("Received: ", words);
                         }
-                    } while (handler.Available > 0);
+                    }
+                    while (handler.Available > 0);
+
                     handler.Shutdown(SocketShutdown.Both);
                     handler.Close();
-                }                
+                }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
